51796914910aa0cae26c944ac134ed92086b9c10
kayobc <71732367+kayobc@users.noreply.github.com>
2020-09-28 12:10:56 -0300

    Update Demo_valida_login.php

--- Demo_valida_login.php
+++ Demo_valida_login.php
@@ -4,19 +4,20 @@ include 'Demo_bd_connnection.php';
 
 $conexaobd = AbreCon();
 $user = $_POST['text_login'];
-$senha = hash("sha256", $_POST['text_senha']);
+$senha = $_POST['text_senha'];
 
 if ($conexaobd -> connect_error) {
   die("Falha na conexao com o banco de dados:" .$conexaobd -> connect_error);
 }
 
-$verifica = mysqli_query($conexaobd, "SELECT * FROM usuario WHERE usuario = '$user' AND senha = '$senha'");
+$verifica = mysqli_query($conexaobd, "SELECT * FROM usuario WHERE usuario = '$user' AND senha = '$senha'") or die("Login e/ou senha incorretos");
 
 if (mysqli_num_rows($verifica)<=0){
-  echo "NOK";
+  echo"/NOK";
+  die();
   }
 else {
-	   echo "OK";
+           echo"/OK";
       }
 
 
