b7f5c41e9caeac31f11cbee09691beefe6078138
kayobc <71732367+kayobc@users.noreply.github.com>
2020-09-28 12:09:53 -0300

    Update Demo_bd_connnection.php

--- Demo_bd_connnection.php
+++ Demo_bd_connnection.php
@@ -1,22 +1,22 @@
 <?php
 
 function AbreCon() {
-	 
- $host = "10.0.3.45";
+
+ $host = "terraform-20200925161409900700000001.cozoaditqsgx.us-east-1.rds.amazonaws.com";
  $user = "root";
  $senha = "root12345678";
  $bd = "Demo_DB";
- 
+
  $conn = new mysqli($host, $user, $senha, $bd) or die("Falha ao conectar BD");
-  
+
  return $conn;
- 
+
  }
- 
+
 function FechaCon($conn) {
-	 
+
  $conn -> close();
- 
+
  }
-   
+
 ?>
